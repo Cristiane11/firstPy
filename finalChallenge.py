4ae1e678399bfd12dee62ff8c67e3b766eedb560
Cristiane Abreu <bjwmcd@yahoo.com>
2025-08-19 10:24:06 -0400

    'update'

---
+++ finalChallenge.py
@@ -0,0 +1,3 @@
+first_names = ["Christian", "Dylan", "Travis", "Katelyn"]
+last_names = ["Sachs", "Katina", "Peck", "Mehner"]
+print(" my name is",first_names[0], last_names[0])
---
+++ first.py
@@ -0,0 +1,16 @@
+
+
+# name = input("What is your name?")
+# age = input("what is your Age?")
+# height = 5.9
+# print(name, age, height)
+
+age = int(input("What is your age?"))
+if age <= 18:
+    print("Your are a minor")
+   
+elif age >= 18:
+    print("You are allow to drink!")
+    
+else:
+    print(" it needs to have at leats 18 to drink alchool")
