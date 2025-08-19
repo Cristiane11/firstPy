ff0edce511a11453b93b265b3e3664208f538429
Cristiane Abreu <bjwmcd@yahoo.com>
2025-08-19 10:51:03 -0400

    update

--- finalChallenge.py
+++ finalChallenge.py
@@ -1,3 +1,9 @@
+import random
 first_names = ["Christian", "Dylan", "Travis", "Katelyn"]
 last_names = ["Sachs", "Katina", "Peck", "Mehner"]
-print(" my name is",first_names[0], last_names[0])
+
+print(" my name is", first_names[0], last_names[0])
+
+f_name = random.choice(first_names)
+l_name =  random.choice(last_names)
+print(f_name  + " " + l_name)
